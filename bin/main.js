23de20b2396c8b6dbeea8e840f5d465e6029df1e
Kris Barrett <krisbarrett@gmail.com>
2014-10-29 20:06:42 -0700

    Added command line arguments

--- bin/main.js
+++ bin/main.js
@@ -1,5 +1,13 @@
+var cli = require('cli');
 var TestRunner = require('../lib/test_runner');
 
-var tr = new TestRunner('/dev/ttyUSB0');
-tr.on('done', process.exit);
-tr.start(2);
+cli.parse({
+  serial: ['s', 'Serial Port', 'path', '/dev/ttyUSB0'],
+  mode: ['m', 'Test Mode', 'number', 0]
+});
+
+cli.main(function(args, options) {
+  var tr = new TestRunner(options.serial);
+  tr.on('done', process.exit);
+  tr.start(options.mode);
+});
